bd82bb1bfdab45f812abab6cf82c105f5f8cab9a
Kailai Xu <kailaix@stanford.edu>
2020-03-14 11:44:54 -0700

    update

--- src/ADCMEKit.jl
+++ src/ADCMEKit.jl
@@ -2,8 +2,18 @@ module ADCMEKit
 
 using PyPlot
 using LinearAlgebra
+import PyCall
 import PyCall:PyObject
 using ADCME
+
+
+animation_ = PyCall.PyNULL()
+function __init__()
+    global animation_
+    copy!(animation_,PyCall.pyimport("matplotlib.animation"))
+end
 include("Core.jl")
+include("Visualization.jl")
+
 
 end # module
---
+++ src/Visualization.jl
@@ -0,0 +1,37 @@
+export animate, saveanim
+
+@doc raw"""
+    animate(update::Function, frames; kwargs...)
+
+Creates an animation using update function `update`. 
+# Example 
+```julia
+θ = LinRange(0, 2π, 100)
+x = cos.(θ)
+y = sin.(θ)
+pl, = plot([], [], "o-")
+t = title("0")
+xlim(-1.2,1.2)
+ylim(-1.2,1.2)
+function update(i)
+    t.set_text("$i")
+    pl.set_data([x[1:i] y[1:i]]'|>Array)
+end
+animate(update, 1:100)
+```
+"""
+function animate(update::Function, frames; kwargs...)
+    if !isa(frames, Integer)
+        frames = Array(frames)
+    end
+    animation_.FuncAnimation(gcf(), update, frames=frames; kwargs...)
+end
+
+"""
+    saveanim(anim::PyObject, filename::String; kwargs...)
+
+Saves the animation produced by [`animate`](@ref)
+"""
+function saveanim(anim::PyObject, filename::String; kwargs...)
+    anim.save(filename, "imagemagick"; kwargs...)
+end
