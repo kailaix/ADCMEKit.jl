8a84266050b0a98b99e416c54ba8f352e59c21d2
Kailai Xu <kailaix@stanford.edu>
2020-04-07 13:13:06 -0700

    add tikzplotlib

--- src/ADCMEKit.jl
+++ src/ADCMEKit.jl
@@ -7,15 +7,14 @@ import PyCall:PyObject
 using ADCME
 
 
-
+mpl = PyCall.PyNULL()
 animation_ = PyCall.PyNULL()
 function __init__()
-    global animation_
+    global animation_, mpl 
     copy!(animation_,PyCall.pyimport("matplotlib.animation"))
+    copy!(mpl,PyCall.pyimport("tikzplotlib"))
 end
 include("Core.jl")
 include("Visualization.jl")
-include("Unit.jl")
-
 
 end # module
--- src/Unit.jl
+++
@@ -1,8 +0,0 @@
-import Unitful
-export Unit, value
-
-Unit = Unitful 
-
-function value(p::Unit.Quantity)
-    return Unit.upreferred(p).val|>float
-end
