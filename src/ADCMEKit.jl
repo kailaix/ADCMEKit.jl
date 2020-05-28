b82375bda4ade90c179fa00629d6333402d4025f
kailaix <kailaix@hotmail.com>
2020-05-28 15:09:01 -0700

    update

--- src/ADCMEKit.jl
+++ src/ADCMEKit.jl
@@ -6,7 +6,6 @@ import PyCall
 import PyCall:PyObject
 using ADCME
 
-export tikz 
 
 tikz = PyCall.PyNULL()
 animation_ = PyCall.PyNULL()
@@ -14,15 +13,6 @@ function __init__()
     global animation_, tikz 
     copy!(animation_,PyCall.pyimport("matplotlib.animation"))
     copy!(tikz,PyCall.pyimport("tikzplotlib"))
-    if Sys.iswindows()
-py"""
-import sys, codecs
-if sys.stdout.encoding != 'cp850':
-  sys.stdout = codecs.getwriter('cp850')(sys.stdout.buffer, 'strict')
-if sys.stderr.encoding != 'cp850':
-  sys.stderr = codecs.getwriter('cp850')(sys.stderr.buffer, 'strict')
-"""
-    end
 end
 include("Core.jl")
 include("Visualization.jl")
