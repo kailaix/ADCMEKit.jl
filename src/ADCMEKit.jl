747649986c481a239b85042063a4906469e62990
Kailai Xu <kailaix@hotmail.com>
2020-09-03 22:25:05 -0700

    update

--- src/ADCMEKit.jl
+++ src/ADCMEKit.jl
@@ -5,7 +5,6 @@ using LinearAlgebra
 import PyCall
 import PyCall:PyObject
 using ADCME
-using Conda
 
 
 tikz = PyCall.PyNULL()
@@ -16,11 +15,11 @@ function __init__()
     copy!(tikz,PyCall.pyimport("tikzplotlib"))
 
     if Sys.iswindows()
-        if !occursin(joinpath(Conda.ROOTENV, "Scripts"), replace(ENV["PATH"], "\\\\"=>"\\"))
-            @warn """$(joinpath(Conda.ROOTENV, "Scripts")) is not in the system path. Some of the ADCMEKit tools may break."""
+        if !occursin(ADCME.BINDIR, replace(ENV["PATH"], "\\\\"=>"\\"))
+            @warn """$(ADCME.BINDIR) is not in the system path. Some of the ADCMEKit tools may break."""
         end
-    elseif !occursin(Conda.BINDIR, ENV["PATH"])
-        @warn """$(Conda.BINDIR) is not in the system path. Some of the ADCMEKit tools may break."""
+    elseif !occursin(ADCME.BINDIR, ENV["PATH"])
+        @warn """$(ADCME.BINDIR) is not in the system path. Some of the ADCMEKit tools may break."""
     end
 end
 include("Core.jl")
